e92a1788ddc3f49b82fad878852babde1f477fef
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-06-16 11:39:19 +0800

    Update Forecasting PDRB64.R

--- Forecasting PDRB64.R
+++ Forecasting PDRB64.R
@@ -1,8 +1,6 @@
 # Penjelasan
 browseURL("https://github.com/idabdulmaj/ForecastPDRB64/blob/Utama/README.md")
 
-# ==========================================================================
-
 # Persiapan - Install Package yang Dibutuhkan
 # Install Package Forecasting PDRB (HANYA UNTUK PERTAMA)
 install.packages("devtools")
