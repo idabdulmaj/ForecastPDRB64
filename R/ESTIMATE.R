3c4aff45d80445dd150342267cb36e1276f74d2c
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-06-16 11:52:37 +0800

    Update ESTIMATE.R

--- R/ESTIMATE.R
+++ R/ESTIMATE.R
@@ -321,6 +321,9 @@ export.hasil <- function(arima.forecastedval, arima.fittedval, es.forecastedval,
   cat("-------------------------------------------------------------------------------------- \n")
   
   cat.final()
+
+  shell.exec(paste0(getwd(), "/4. Output R/Hasil Forecasting ARIMA dan EXPONENTIAL SMOOTHING.xlsx"))
+  shell.exec(paste0(getwd(), "/5. Hasil Forecast/64 Kaltim Data Forecast COMPILE.xlsx"))
 }
 
 forecast.pdrb.64 <- function(data_df){
