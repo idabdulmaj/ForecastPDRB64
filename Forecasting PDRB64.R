05d77a4165be9ebb1352f02f2fd3617928e6c172
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-06-16 11:04:59 +0800

    Update Forecasting PDRB64.R

--- Forecasting PDRB64.R
+++ Forecasting PDRB64.R
@@ -1,30 +1,25 @@
-# ==========================================================================
 # Penjelasan
-# --------------------------------------------------------------------------
 browseURL("https://github.com/idabdulmaj/ForecastPDRB64/blob/Utama/README.md")
 
-# --------------------------------------------------------------------------
+# ==========================================================================
+
+# Persiapan - Install Package yang Dibutuhkan
 # Install Package Forecasting PDRB (HANYA UNTUK PERTAMA)
 install.packages("devtools")
 devtools::install_github("idabdulmaj/ForecastPDRB64")
-# ==========================================================================
-
 
 # ==========================================================================
-# Persiapan - Mengatur Working Directory dan Library Loading
-# --------------------------------------------------------------------------
+
+# Persiapan - Mengatur Working Directory
 setwd(choose.dir())
+
+# Library yang Dibutuhkan
 library(ForecastPDRB64)
 cek.package.nya()
 
-# --------------------------------------------------------------------------
 # Data Loading
-# --------------------------------------------------------------------------
 pdrb_df <- read.xlsx("1. Data Input/pdrb.xlsx")
 data.pdrb <- pdrb_df[, 3:ncol(pdrb_df)]
 
-# --------------------------------------------------------------------------
 # Forecasting PDRB
-# --------------------------------------------------------------------------
 suppressWarnings(forecast.pdrb.64(data.pdrb))
-# ==========================================================================
