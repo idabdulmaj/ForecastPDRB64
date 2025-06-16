71dbda9d9ebe8f884bb6b7e43893277e63fbcdf1
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-06-16 11:34:03 +0800

    Update Forecasting PDRB64.R

--- Forecasting PDRB64.R
+++ Forecasting PDRB64.R
@@ -11,7 +11,7 @@ devtools::install_github("idabdulmaj/ForecastPDRB64")
 # ==========================================================================
 
 # Persiapan - Mengatur Working Directory
-dir.path <- choose.dir(caption = "Pilih Folder untuk Working Directory")
+dir.path <- dirname(file.choose())
 setwd(dir.path)
 
 # Library yang Dibutuhkan
