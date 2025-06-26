d646b2e69dc4ddc208bb9c0d470949de51c48d8a
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-06-26 07:19:30 +0000

    penyesuaian hint

--- R/ESTIMATE.R
+++ R/ESTIMATE.R
@@ -381,5 +381,5 @@ buka.hasil <- function(){
   cat("Hasil Forecasting akan terbuka secara otomatis di Excel. \n")
   cat("Jika tidak terbuka, silakan buka file tersebut secara manual. \n")
   cat("-------------------------------------------------------------------------------------- \n")
-  cat("Silakan masukan file Hasil Forecasting ke file 64 Kaltim Data Forecast COMPILE.xlsx untuk melihat hasil forecast secara lengkap. \n")
+  cat("Silakan masukan file Hasil Forecasting ke file 64 Kaltim Data Forecast COMPILE.xlsx untuk melihat hasil forecast secara lengkap. Jika ada yang perlu dilakukan penambahan perlakuan seasonal, dapat menghubungi pembuat package. \n")
 }
