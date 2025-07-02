bd5f50bc3323a4274f564b1f94687c079e04f648
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-07-02 07:40:54 +0000

    refactor: improve output formatting in export.hasil and forecast.pdrb.64 functions

--- R/ESTIMATE.R
+++ R/ESTIMATE.R
@@ -197,7 +197,7 @@ export.hasil <- function(arima.forecastedval, arima.fittedval, es.forecastedval,
   file_path <- file.path("4. Output R/Hasil Forecasting ARIMA dan EXPONENTIAL SMOOTHING.xlsx")
   write.xlsx(savetoexcel, file = file_path)
 
-  cat("-------------------------------------------------------------------------------------- \n")
+  cat("\n-------------------------------------------------------------------------------------- \n")
   cat("File Excel Forcasted Value dan Fitted Value telah disimpan di Folder 4. Output R \n")
   cat("-------------------------------------------------------------------------------------- \n")
   
@@ -217,8 +217,8 @@ forecast.pdrb.64 <- function(data_df, seasonal_df) {
   es <- pdrb.forecast.es(data.pdrb, seasonal_df)
   sink()
 
-  cat("-------------------------------------------------------------------------------------- \n")
-  cat("\nModel dan Plot ARIMA disimpan pada folder 2. ARIMA Plot dan Model \n")
+  cat("\n-------------------------------------------------------------------------------------- \n")
+  cat("Model dan Plot ARIMA disimpan pada folder 2. ARIMA Plot dan Model \n")
   cat("-------------------------------------------------------------------------------------- \n")
   cat("Model dan Plot Exponential Smoothing disimpan pada 3. Exp Smoothing Plot dan Model \n")
   cat("-------------------------------------------------------------------------------------- \n")
