bdf6126bc0de21bd27b4cfa2c8a3f30e4dedb908
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-07-02 07:21:50 +0000

    refactor: rename forecasting functions and update parameters for consistency

--- R/ESTIMATE.R
+++ R/ESTIMATE.R
@@ -42,7 +42,7 @@ cek.package.nya <- function() {
   cat("\n Seluruh package yang dibutuhkan telah di-Load.\n")
 }
 
-pdrb.forecast.arima.lapus <- function(data_df) {
+pdrb.forecast.arima <- function(data_df, seasonal_df) {
   # INISIASI
   forecasted_df <- data.frame()
   fitted_df <- data.frame()
@@ -52,7 +52,7 @@ pdrb.forecast.arima.lapus <- function(data_df) {
   # Melakukan forecasting untuk setiap variabel
   for (i in 1:ncol(data_df)) {
     # Melakukan arima dengan parameter yang ditentukan
-    if (i != 3) {
+    if (seasonal_df[i, 2] != 3) {
       prediksi <- auto.arima(data_df[, i])
     } else {
       ts_data <- ts(data_df[, i], start = 1, frequency = 4)
@@ -107,7 +107,7 @@ pdrb.forecast.arima.lapus <- function(data_df) {
   return(list(forecastedval = forecasted_df, fittedval = fitted_df))
 }
 
-pdrb.forecast.es.lapus <- function(data_df) {
+pdrb.forecast.es <- function(data_df, seasonal_df) {
   # INISIASI
   forecasted_df <- data.frame()
   fitted_df <- data.frame()
@@ -120,7 +120,7 @@ pdrb.forecast.es.lapus <- function(data_df) {
     ts_data <- ts(data_df[, i], start = 1, frequency = 4)
 
     # Melakukan exponential smoothing dengan parameter yang ditentukan
-    if (i != 3) {
+    if (seasonal_df[i, 2] != 3) {
       prediksi <- holt(ts_data, damped = TRUE, alpha = NULL, beta = NULL)
     } else {
       prediksi <- ets(ts_data)
@@ -172,127 +172,6 @@ pdrb.forecast.es.lapus <- function(data_df) {
   return(list(forecastedval = forecasted_df, fittedval = fitted_df))
 }
 
-pdrb.forecast.arima.pengeluaran <- function(data_df) {
-  # INISIASI
-  forecasted_df <- data.frame()
-  fitted_df <- data.frame()
-  fitted_val <- data.frame()
-  plot_list <- list()
-
-  # Melakukan forecasting untuk setiap variabel
-  for (i in 1:ncol(data_df)) {
-    # Melakukan arima dengan parameter yang ditentukan
-    ts_data <- ts(data_df[, i], start = 1, frequency = 4)
-    prediksi <- auto.arima(ts_data)
-
-    # Melakukan forecasting
-    forecasted_values <- forecast(prediksi, h = 1)
-
-    # Menambahkan hasil forecasting ke dalam dataframe
-    forecasted_df[1, i] <- forecasted_values$mean
-    forecasted_df[2, i] <- forecasted_values$upper
-    forecasted_df[3, i] <- forecasted_values$lower
-
-    # Menambahkan hasil fitted ke dalam dataframe
-    fitted_val <- ts(forecasted_values$fitted, frequency = 1)
-    for (j in 1:nrow(data_df)) {
-      fitted_df[j, i] <- fitted_val[j]
-    }
-
-    # Menyimpan hasil forecast dalam plot
-    mypath <- file.path("2. ARIMA Plot dan Model", paste0("ARIMA - ", i, ". ", colnames(data_df[i]), ".png"))
-    png(mypath)
-    plot_list[i] <- plot(forecasted_values, col = "red", main = paste0(colnames(data_df[i])), ylab = "PDRB", xlab = "Triwulan ke")
-    plot_list[i] <- lines(forecasted_values$fitted, pch = 20, col = "green")
-    plot_list[i] <- legend("topleft", c("Actual data", "Smoothed data"), lty = 8, col = c("green", "red"), cex = 0.8)
-    dev.off()
-
-    # Mencetak hasil forecast dalam list
-    cat("-----------------------------------------------------------------------", "\n")
-    cat(colnames(data_df)[i], "\n")
-    cat("-----------------------------------------------------------------------", "\n")
-    print(summary(prediksi))
-    cat("\n", "\n")
-
-  }
-
-  # Save forecast
-  names(forecasted_df) <- names(data_df)
-  # Transpose
-  forecasted_df <- t(forecasted_df)
-  colnames(forecasted_df) <- c("Mean", "Upper", "Lower")
-  forecasted_df <- as.data.frame(forecasted_df)
-  # Tambah Kolom
-  forecasted_df <- tibble::rownames_to_column(forecasted_df, "Kategori_Subkategori")
-
-  # Save fitted
-  names(fitted_df) <- names(data_df)
-  fitted_df <- data.frame(pdrb_df[, 1:2], fitted_df)
-
-  # Save Output
-  return(list(forecastedval = forecasted_df, fittedval = fitted_df))
-}
-
-pdrb.forecast.es.pengeluaran <- function(data_df) {
-  # INISIASI
-  forecasted_df <- data.frame()
-  fitted_df <- data.frame()
-  fitted_val <- data.frame()
-  plot_list <- list()
-
-  # Melakukan forecasting untuk setiap variabel
-  for (i in 1:ncol(data_df)) {
-    # Mengambil data time series
-    ts_data <- ts(data_df[, i], start = 1, frequency = 4)
-
-    # Melakukan exponential smoothing dengan parameter yang ditentukan
-    prediksi <- ets(ts_data)
-
-    # Melakukan forecasting
-    forecasted_values <- forecast(prediksi, h = 1)
-
-    # Menambahkan hasil forecasting ke dalam dataframe
-    forecasted_df[1, i] <- forecasted_values$mean
-    forecasted_df[2, i] <- forecasted_values$upper
-    forecasted_df[3, i] <- forecasted_values$lower
-
-    # Menambahkan hasil fitted ke dalam dataframe
-    fitted_val <- ts(forecasted_values$fitted, frequency = 1)
-    for (j in 1:nrow(data_df)) {
-      fitted_df[j, i] <- fitted_val[j]
-    }
-
-    # Menyimpan hasil forecast dalam plot
-    mypath <- file.path("3. Exp Smoothing Plot dan Model", paste0("Exp Smoothing - ", i, ". ", colnames(data_df[i]), ".png"))
-    png(mypath)
-    plot_list[i] <- plot(forecasted_values, col = "red", main = paste0(colnames(data_df[i])), ylab = "PDRB", xlab = "Triwulan ke")
-    plot_list[i] <- lines(forecasted_values$fitted, pch = 20, col = "green")
-    plot_list[i] <- legend("topleft", c("Actual data", "Smoothed data"), lty = 8, col = c("green", "red"), cex = 0.8)
-    dev.off()
-
-    # Mencetak hasil forecast dalam list
-    cat(colnames(data_df)[i], "\n")
-    cat("-----------------------------------------------------------------------", "\n")
-    print(summary(prediksi))
-    cat("\n", "\n")
-  }
-
-  # Save forecast
-  names(forecasted_df) <- names(data_df)
-  # Transpose
-  forecasted_df <- t(forecasted_df)
-  colnames(forecasted_df) <- c("Mean", "Upper", "Lower")
-  forecasted_df <- as.data.frame(forecasted_df)
-  # Tambah Kolom
-  forecasted_df <- tibble::rownames_to_column(forecasted_df, "Kategori_Subkategori")
-
-  # Save fitted
-  names(fitted_df) <- names(data_df)
-  fitted_df <- data.frame(pdrb_df[, 1:2], fitted_df)
-
-  # Save Output
-  return(list(forecastedval = forecasted_df, fittedval = fitted_df))
-}
 
 cat.final <- function(){
   catlist <- c("bongo", "colonel", "grumpy", "hipster", "lil_bub", "maru", "mouth", "pop", "pop_close",
