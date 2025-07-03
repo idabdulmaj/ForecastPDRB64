fdda95551b0403210475c41b2f7119eac601aac0
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2025-07-03 06:37:03 +0000

    refactor: simplify library loading process in check_and_load_ForecastPDRB64 function for improved clarity

--- Forecasting PDRB64.R
+++ Forecasting PDRB64.R
@@ -7,12 +7,11 @@ check_and_load_ForecastPDRB64 <- function() {
   if (!require("ForecastPDRB64", character.only = TRUE)) {
     if (!require("devtools", character.only = TRUE)) {
       install.packages("devtools")
-      library(devtools)
     }
-    devtools::install_github("idabdulmaj/ForecastPDRB64")
-    library(ForecastPDRB64, character.only = TRUE)
-    cek.package.nya()
+    devtools::install_github("idabdulmaj/ForecastPDRB64") 
   }
+  library(ForecastPDRB64)
+  cek.package.nya()
 }
 check_and_load_ForecastPDRB64()
 
