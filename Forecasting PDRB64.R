0ec4eaa8fe62aba20068a0672b91efe86d8f23db
Muhammad Abdul Majid <34195311+idabdulmaj@users.noreply.github.com>
2026-04-07 06:21:31 +0000

    update: rename cek.package.nya function to package.check for clarity

--- Forecasting PDRB64.R
+++ Forecasting PDRB64.R
@@ -8,7 +8,7 @@ setwd(dirname(file.choose()))
 
 # Load the package and check if it's working ===================================
 library(ForecastPDRB64)
-cek.package.nya()
+package.check()
 
 # Import Data & Forecasting PDRB ===============================================
 data_pdrb <- load.data.pdrb()
